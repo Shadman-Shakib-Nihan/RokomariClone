4be50fcd3a1819c8826f23fd81cca813f68beda5
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 17:58:40 +0600

    adminRoute

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\BrandController;
+use App\Http\Controllers\Admin\PublisherController;
+use App\Http\Controllers\Admin\AuthorController;
+use App\Http\Controllers\Admin\AttributeController;
+use App\Http\Controllers\Admin\AttributeOptionController;
+use App\Http\Controllers\Admin\ProductController;
+
 
 Route::inertia('/', 'Welcome')->name('home');
 
@@ -8,5 +16,26 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
 });
 Route::inertia('show', 'Show')->name('show');
+
+Route::prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+
+        Route::resource('categories', CategoryController::class);
+
+        Route::resource('brands', BrandController::class);
+
+        Route::resource('publishers', PublisherController::class);
+
+        Route::resource('authors', AuthorController::class);
+
+        Route::resource('attributes', AttributeController::class);
+
+        Route::resource('attribute-options', AttributeOptionController::class);
+
+        Route::resource('products', ProductController::class);
+
+    });
+
 
 require __DIR__.'/settings.php';
