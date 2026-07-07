4514fc91dc70b5a6616cfd65461b01f1686ef1f2
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:49:15 +0600

    variant

--- app/Models/VariantAttributeValue.php
+++ app/Models/VariantAttributeValue.php
@@ -3,8 +3,34 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class VariantAttributeValue extends Model
 {
-    //
+    protected $fillable = [
+        'product_variant_id',
+        'attribute_option_id',
+    ];
+
+    /**
+     * Product Variant
+     */
+    public function variant(): BelongsTo
+    {
+        return $this->belongsTo(
+            ProductVariant::class,
+            'product_variant_id'
+        );
+    }
+
+    /**
+     * Selected Attribute Option
+     */
+    public function option(): BelongsTo
+    {
+        return $this->belongsTo(
+            AttributeOption::class,
+            'attribute_option_id'
+        );
+    }
 }
