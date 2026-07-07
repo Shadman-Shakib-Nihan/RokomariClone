5f0733a57162f2d1106f4891d245a14cdcb2ee5f
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:41:37 +0600

    publisher Model

--- app/Models/Publisher.php
+++ app/Models/Publisher.php
@@ -3,8 +3,31 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Publisher extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'logo',
+        'website',
+        'is_active',
+    ];
+
+    protected $casts = [
+        'is_active' => 'boolean',
+    ];
+
+    /**
+     * Books published by this publisher
+     */
+    public function products(): HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
 }
