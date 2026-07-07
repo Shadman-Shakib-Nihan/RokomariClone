e5d799e41a7e92b2bc3b5c2b8a146282022643f1
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:47:38 +0600

    category 2

--- app/Models/CategoryAttribute.php
+++ app/Models/CategoryAttribute.php
@@ -3,8 +3,36 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class CategoryAttribute extends Model
 {
-    //
+    protected $fillable = [
+        'category_id',
+        'attribute_id',
+        'is_required',
+        'is_filterable',
+        'sort_order',
+    ];
+
+    protected $casts = [
+        'is_required' => 'boolean',
+        'is_filterable' => 'boolean',
+    ];
+
+    /**
+     * Category
+     */
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    /**
+     * Attribute
+     */
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
 }
