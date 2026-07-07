c4bdadfe90a2e51298e51fe234e3f891efcb32c9
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:48:26 +0600

    product 2

--- app/Models/ProductAttributeValue.php
+++ app/Models/ProductAttributeValue.php
@@ -3,8 +3,47 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductAttributeValue extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'attribute_id',
+        'attribute_option_id',
+        'value_text',
+        'value_number',
+        'value_boolean',
+        'value_date',
+    ];
+
+    protected $casts = [
+        'value_boolean' => 'boolean',
+        'value_number' => 'decimal:2',
+        'value_date' => 'date',
+    ];
+
+    /**
+     * Product
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Attribute
+     */
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    /**
+     * Selected Option
+     */
+    public function option(): BelongsTo
+    {
+        return $this->belongsTo(AttributeOption::class, 'attribute_option_id');
+    }
 }
