4193d67b06e63ec622338b634c1ec2c775e1e563
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:43:34 +0600

    add

--- app/Models/ProductAuthor.php
+++ app/Models/ProductAuthor.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ProductAuthor extends Model
 {
-    //
+    protected $fillable = [
+        'product_id',
+        'author_id',
+        'sort_order',
+    ];
+
+    /**
+     * Book
+     */
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+    /**
+     * Author
+     */
+    public function author(): BelongsTo
+    {
+        return $this->belongsTo(Author::class);
+    }
 }
