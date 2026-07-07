05fbeab4e365177a4bc82f3449d0c938fa6453e6
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 15:44:23 +0600

    variantImage

--- app/Models/VariantImage.php
+++ app/Models/VariantImage.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class VariantImage extends Model
 {
-    //
+    protected $fillable = [
+        'product_variant_id',
+        'url',
+        'sort_order',
+        'is_primary',
+    ];
+
+    protected $casts = [
+        'is_primary' => 'boolean',
+    ];
+
+    /**
+     * Product Variant
+     */
+    public function variant(): BelongsTo
+    {
+        return $this->belongsTo(ProductVariant::class, 'product_variant_id');
+    }
 }
