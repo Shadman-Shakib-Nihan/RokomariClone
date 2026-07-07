8dc6dab4c6299b34623d35beac2aaa26bbe278bf
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 14:37:48 +0600

    Brand model

--- app/Models/Brand.php
+++ app/Models/Brand.php
@@ -3,8 +3,27 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Brand extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'logo',
+        'description',
+        'website',
+        'is_active',
+    ];
+
+    protected $casts=[
+        'is_active'=>'boolean',
+    ];
+
+    public function products(){
+        return $this->hasMany(Product::class);
+    }
 }
