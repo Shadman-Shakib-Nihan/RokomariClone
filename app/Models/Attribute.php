ea35aef1e3b2f5bdf46409ce829c916b211956d9
Shadman-Shakib-Nihan <nihanshadman1@gmail.com>
2026-07-07 14:59:35 +0600

    attributeOption

--- app/Models/Attribute.php
+++ app/Models/Attribute.php
@@ -3,8 +3,38 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Attribute extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'slug',
+        'input_type',
+        'unit',
+    ];
+
+    /**
+     * Dropdown options
+     */
+    public function options(): HasMany
+    {
+        return $this->hasMany(AttributeOption::class);
+    }
+
+    /**
+     * Categories using this attribute
+     */
+    public function categoryAttributes(): HasMany
+    {
+        return $this->hasMany(CategoryAttribute::class);
+    }
+
+    /**
+     * Product values
+     */
+    public function productValues(): HasMany
+    {
+        return $this->hasMany(ProductAttributeValue::class);
+    }
 }
--- app/Models/AttributeOption.php
+++ app/Models/AttributeOption.php
@@ -3,8 +3,40 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AttributeOption extends Model
 {
-    //
+    protected $fillable = [
+        'attribute_id',
+        'value',
+        'sort_order',
+        'color_hex',
+        'image',
+    ];
+
+    /**
+     * Parent attribute
+     */
+    public function attribute(): BelongsTo
+    {
+        return $this->belongsTo(Attribute::class);
+    }
+
+    /**
+     * Variant values using this option
+     */
+    public function variantValues(): HasMany
+    {
+        return $this->hasMany(VariantAttributeValue::class);
+    }
+
+    /**
+     * Product values using this option
+     */
+    public function productValues(): HasMany
+    {
+        return $this->hasMany(ProductAttributeValue::class);
+    }
 }
